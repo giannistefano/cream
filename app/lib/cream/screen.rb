0eefdbdcac051b93a00cbd925e911f907916cb18
Gianni Stefano <gianni.stefano@gmail.com>
2014-04-23 19:51:04 +0200

    calculator correction

--- app/lib/cream/screen.rb
+++ app/lib/cream/screen.rb
@@ -33,90 +33,91 @@ $svcalc = '0'
 #Tk::Tile::Label.new(content) {text $lv_sel_user}.grid( :column => 1, :row => 1, :sticky => 'we');
 
 
-# create the calculator inputfield 
-tl_calc = Tk::Tile::Entry.new(content) {width 7; textvariable $currvalue}.grid( :columnspan => 3, :row => 0, :sticky => 'we')
+# create the calculator inputfield overview 
+tl_overview = Tk::Tile::Entry.new(content) {width 7; textvariable $overview}.grid( :columnspan => 4, :row => 0, :sticky => 'we')
+# create the calculator inputfield result 
+tl_result = Tk::Tile::Entry.new(content) {width 7; textvariable $currvalue}.grid( :columnspan => 3, :row => 1, :sticky => 'we')
 
 # create the button with value backspace
-Tk::Tile::Button.new(content) {text '<-'; command {delete1digit}}.grid( :column => 3, :row => 0, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '<-'; command {delete1digit}}.grid( :column => 3, :row => 1, :sticky => 'w')
 
 # create the button with value 1
-Tk::Tile::Button.new(content) {text '1'; command {add1digit('1')}}.grid( :column => 0, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '1'; command {add1digit('1')}}.grid( :column => 0, :row => 2, :sticky => 'w')
 # create the button with value 2
-Tk::Tile::Button.new(content) {text '2'; command {add1digit('2')}}.grid( :column => 1, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '2'; command {add1digit('2')}}.grid( :column => 1, :row => 2, :sticky => 'w')
 # create the button with value 3
-Tk::Tile::Button.new(content) {text '3'; command {add1digit('3')}}.grid( :column => 2, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '3'; command {add1digit('3')}}.grid( :column => 2, :row => 2, :sticky => 'w')
 # create the button with value %
-Tk::Tile::Button.new(content) {text '%'; command {setaction('%')}}.grid( :column => 3, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '%'; command {setaction('%')}}.grid( :column => 3, :row => 2, :sticky => 'w')
 
 # create the button with value 4
-Tk::Tile::Button.new(content) {text '4'; command {add1digit('4')}}.grid( :column => 0, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '4'; command {add1digit('4')}}.grid( :column => 0, :row => 3, :sticky => 'w')
 # create the button with value 5
-Tk::Tile::Button.new(content) {text '5'; command {add1digit('5')}}.grid( :column => 1, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '5'; command {add1digit('5')}}.grid( :column => 1, :row => 3, :sticky => 'w')
 # create the button with value 6
-Tk::Tile::Button.new(content) {text '6'; command {add1digit('6')}}.grid( :column => 2, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '6'; command {add1digit('6')}}.grid( :column => 2, :row => 3, :sticky => 'w')
 # create the button with value -
-Tk::Tile::Button.new(content) {text 'x'; command {setaction('x')}}.grid( :column => 3, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(content) {text 'x'; command {setaction('x')}}.grid( :column => 3, :row => 3, :sticky => 'w')
 
 # create the button with value 7
-Tk::Tile::Button.new(content) {text '7'; command {add1digit('7')}}.grid( :column => 0, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '7'; command {add1digit('7')}}.grid( :column => 0, :row => 4, :sticky => 'w')
 # create the button with value 8
-Tk::Tile::Button.new(content) {text '8'; command {add1digit('8')}}.grid( :column => 1, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '8'; command {add1digit('8')}}.grid( :column => 1, :row => 4, :sticky => 'w')
 # create the button with value 9
-Tk::Tile::Button.new(content) {text '9'; command {add1digit('9')}}.grid( :column => 2, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '9'; command {add1digit('9')}}.grid( :column => 2, :row => 4, :sticky => 'w')
 # create the button with value x
-Tk::Tile::Button.new(content) {text '-'; command {setaction('-')}}.grid( :column => 3, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '-'; command {setaction('-')}}.grid( :column => 3, :row => 4, :sticky => 'w')
 
 # create the button with value 0
-Tk::Tile::Button.new(content) {text '0'; command {add1digit('0')}}.grid( :column => 0, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '0'; command {add1digit('0')}}.grid( :column => 0, :row => 5, :sticky => 'w')
 # create the button with value ,
-Tk::Tile::Button.new(content) {text ','; command {add1digit('.')}}.grid( :column => 1, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(content) {text ','; command {add1digit('.')}}.grid( :column => 1, :row => 5, :sticky => 'w')
 # create the button with value =
-Tk::Tile::Button.new(content) {text '='; command {calcresult}}.grid( :column => 2, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '='; command {setaction('=')}}.grid( :column => 2, :row => 5, :sticky => 'w')
 # create the button with value +
-Tk::Tile::Button.new(content) {text '+'; command {setaction('+')}}.grid( :column => 3, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(content) {text '+'; command {setaction('+')}}.grid( :column => 3, :row => 5, :sticky => 'w')
+
+# create the button with value c
+Tk::Tile::Button.new(content) {text 'c'; command {setaction('c')}}.grid( :column => 1, :row => 6, :sticky => 'w')
 
 TkWinfo.children(content).each {|w| TkGrid.configure w, :padx => 5, :pady => 5}
-tl_calc.focus
+tl_overview.focus
 enteramountscreen.bind("Return") {calcresult}
+
 
 def calcresult
   begin
-     # add a comment
      puts 'calculate result'
-     #$currvalue.value = 0
      $currvaluereset = 'y'
-     $svcalc = 0
      $commaset = ' '
      puts 'act to perform ' + $actoperf
-     #$tl_calc.value = 'result'
+     #$tl_overview.value = 'result'
      puts 'previous value <' + $prevvalue.to_s + '>'
-     if $prevvalue == 0
-        puts 'previous value is 0'
-        #$prevvalue = $svcalc
-        $prevvalue.value = $currvalue
-        puts '1 prev value change to ' + $prevvalue.to_s
-     else     
-        if $actoperf == 'add' 
-           puts 'adding ' + $prevvalue.to_s + ' to ' + $currvalue.to_s
-           $svcalc = $prevvalue + $currvalue
-        end
-        if $actoperf == 'subtract' 
-           puts 'subtracting'
-           $svcalc = $prevvalue - $currvalue
-        end
-        if $actoperf == 'multiply' 
-           puts 'multiplying'
-           $svcalc = $prevvalue * $currvalue
-        end
-        if $actoperf == 'divide' 
-           puts 'dividing'
-           $svcalc = $prevvalue / $currvalue 
-        end
-        puts '2 prev value change to ' + $svcalc.to_s 
-     end       
+     if $actoperf == 'add' 
+        puts 'adding ' + $prevvalue.to_s + ' to ' + $currvalue.to_s
+        $svcalc = $prevvalue + $currvalue
+     end
+     if $actoperf == 'subtract' 
+        puts 'subtracting'
+        $svcalc = $prevvalue - $currvalue
+     end
+     if $actoperf == 'multiply' 
+        puts 'multiplying'
+        $svcalc = $prevvalue * $currvalue
+     end
+     if $actoperf == 'divide' 
+        puts 'dividing'
+        $svcalc = $prevvalue / $currvalue 
+     end
+     
+     puts 'current value change to ' + $svcalc.to_s 
+     $currvalue.value = $svcalc
+        
+     $prevvalue.value = $currvalue
+     puts '2 prev value change to ' + $prevvalue.to_s     
   rescue
-     puts 'calculate result error <' + $svcalc +'>'
-     #$tl_calc.value = 'rescue 1'
+     puts 'calculate result error <' + $svcalc.to_s + '>'
+     #$tl_overview.value = 'rescue 1'
   end
 end
 
@@ -140,25 +141,24 @@ def add1digit(digit)
        end   
        $currvalue.value = $svcalc
        
-       puts $svcalc 
-       puts '3 prev value value ' + $svcalc.to_s 
+       puts 'svcalc value value ' + $svcalc.to_s 
      end   
   rescue
      puts 'add 1 digit error'
-     puts 'prevvalue ' + $prevvalue
-     puts 'currvalue ' + $currvalue
-     puts 'commaset ' +$commaset
-     puts 'svcalc ' + $svcalc
+     puts 'prevvalue ' + $prevvalue.to_s 
+     puts 'currvalue ' + $currvalue.to_s 
+     puts 'commaset ' +$commaset.to_s 
+     puts 'svcalc ' + $svcalc.to_s 
   end
 end
 
 def delete1digit
   begin
      puts 'delete 1 digit'
-     #$tl_calc.value = 'Stefano'
+     #$tl_overview.value = 'Stefano'
   rescue
      puts 'delete 1 digit error'
-     #$tl_calc.value = 'rescue 3'
+     #$tl_overview.value = 'rescue 3'
   end
 end
 
@@ -166,23 +166,37 @@ def setaction(action)
   begin 
      puts 'setaction <' + action + '>'
      $actoperf = case action
+     when 'c'
+       resetcalc
+     when '='
+       calcresult
      when '+'
+       calcresult
        $actoperf = 'add'
      when '-'
+       calcresult
        $actoperf = 'subtract'
      when 'x'
+       calcresult
        $actoperf = 'multiply'
      when '%' 
+       calcresult
        $actoperf = 'divide'
+     when '='
+       calcresult
      else
        $actoperf = 'unknown action'
      end 
      puts $actoperf
-     #$tl_calc.value = 'result'
-     calcresult
-#  rescue
-#     puts 'set action error'
-     #$tl_calc.value = 'rescue 1'
+  end
+end
+
+def resetcalc
+  begin
+    $prevvalue.value = 0
+    $currvalue.value = 0
+    $svcalc = 0
+    $currvaluereset = 'y'
   end
 end
 
