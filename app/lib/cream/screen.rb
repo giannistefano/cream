6b645f5eefb20de445a2f8ecef794ad7138eee82
Gianni Stefano <gianni.stefano@gmail.com>
2014-08-17 15:34:13 +0200

    add tabs and set focus to kassatab
    tabs toegevoegd
    kassatab krijgt de eerste focus

--- app/lib/cream/screen.rb
+++ app/lib/cream/screen.rb
@@ -1,7 +1,7 @@
 require 'tk'
 require 'tkextlib/tile'
 
-enteramountscreen = TkRoot.new {title "Calculator"}
+enteramountscreen = TkRoot.new {title "C.R.E.A.M."}
 content = Tk::Tile::Frame.new(enteramountscreen) {padding "3 3 12 12"}.grid( :sticky => 'nsew')
 TkGrid.columnconfigure enteramountscreen, 0, :weight => 1; TkGrid.rowconfigure enteramountscreen, 0, :weight => 1
 
@@ -30,76 +30,96 @@ $svcalc = '0'
 # screens are built top-down left-right
 # create all widgets that will populate the frame - start
 
-# create the label select user tile 
-#Tk::Tile::Label.new(content) {text $lv_sel_user}.grid( :column => 1, :row => 1, :sticky => 'we');
+# create notebook style frame
+notebook = Tk::Tile::Notebook.new(content).grid( :column => 0, :row => 0, :sticky => 'w')
 
+# create tab 1 - cash register tab
+kassatab = Tk::Tile::Frame.new(notebook); # page
+
+# create tab 2 - user management tab
+userstab = Tk::Tile::Frame.new(notebook); # page
+
+#add tabs to notebookframe
+notebook.add userstab, :text => 'Gebruikers'
+notebook.add kassatab, :text => 'Kassa'
+
+# ----------------------------------------------------------------------------------------
+# populate kassatab ----------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
 # create the button with consumptions to be added:
-Tk::Tile::Button.new(content) {text '+ bier'; command {manageconsumption('1.60','+','bier')}}.grid( :column => 0, :row => 0, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '+ bier'; command {manageconsumption('1.60','+','bier')}}.grid( :column => 0, :row => 0, :sticky => 'w')
 
-Tk::Tile::Button.new(content) {text '+ cola'; command {manageconsumption('2.55','+', 'cola')}}.grid( :column => 1, :row => 0, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '+ cola'; command {manageconsumption('2.55','+', 'cola')}}.grid( :column => 1, :row => 0, :sticky => 'w')
 
-Tk::Tile::Button.new(content) {text '+ wijn'; command {manageconsumption('0.25','+', 'wijn')}}.grid( :column => 2, :row => 0, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '+ wijn'; command {manageconsumption('0.25','+', 'wijn')}}.grid( :column => 2, :row => 0, :sticky => 'w')
 
-Tk::Tile::Button.new(content) {text '+ water'; command {manageconsumption('400.68','+', 'water')}}.grid( :column => 3, :row => 0, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '+ water'; command {manageconsumption('400.68','+', 'water')}}.grid( :column => 3, :row => 0, :sticky => 'w')
 
 # create the button with consumptions to be subtracted:
-Tk::Tile::Button.new(content) {text '- bier'; command {manageconsumption('1.60','-','bier')}}.grid( :column => 0, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '- bier'; command {manageconsumption('1.60','-','bier')}}.grid( :column => 0, :row => 1, :sticky => 'w')
 
-Tk::Tile::Button.new(content) {text '- cola'; command {manageconsumption('2.55','-','cola')}}.grid( :column => 1, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '- cola'; command {manageconsumption('2.55','-','cola')}}.grid( :column => 1, :row => 1, :sticky => 'w')
 
-Tk::Tile::Button.new(content) {text '- wijn'; command {manageconsumption('0.25','-','wijn')}}.grid( :column => 2, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '- wijn'; command {manageconsumption('0.25','-','wijn')}}.grid( :column => 2, :row => 1, :sticky => 'w')
 
-Tk::Tile::Button.new(content) {text '- water'; command {manageconsumption('400.68','-','water')}}.grid( :column => 3, :row => 1, :sticky => 'w')
-
+Tk::Tile::Button.new(kassatab) {text '- water'; command {manageconsumption('400.68','-','water')}}.grid( :column => 3, :row => 1, :sticky => 'w')
 
 # create the calculator inputfield overview 
-tl_overview = Tk::Tile::Entry.new(content) {width 7; textvariable $overview}.grid( :column => 4, :columnspan => 4, :row => 0, :sticky => 'we')
+tl_overview = Tk::Tile::Entry.new(kassatab) {width 7; textvariable $overview}.grid( :column => 4, :columnspan => 4, :row => 0, :sticky => 'we')
 # create the calculator inputfield result 
-tl_result = Tk::Tile::Entry.new(content) {width 7; textvariable $currvalue}.grid( :column => 4, :columnspan => 3, :row => 1, :sticky => 'we')
+tl_result = Tk::Tile::Entry.new(kassatab) {width 7; textvariable $currvalue}.grid( :column => 4, :columnspan => 3, :row => 1, :sticky => 'we')
 
 # create the button with value backspace
-Tk::Tile::Button.new(content) {text '<-'; command {delete1digit}}.grid( :column => 7, :row => 1, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '<-'; command {delete1digit}}.grid( :column => 7, :row => 1, :sticky => 'w')
 
 # create the button with value 1
-Tk::Tile::Button.new(content) {text '1'; command {managedigit('+','1')}}.grid( :column => 4, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '1'; command {managedigit('+','1')}}.grid( :column => 4, :row => 2, :sticky => 'w')
 # create the button with value 2
-Tk::Tile::Button.new(content) {text '2'; command {managedigit('+','2')}}.grid( :column => 5, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '2'; command {managedigit('+','2')}}.grid( :column => 5, :row => 2, :sticky => 'w')
 # create the button with value 3
-Tk::Tile::Button.new(content) {text '3'; command {managedigit('+','3')}}.grid( :column => 6, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '3'; command {managedigit('+','3')}}.grid( :column => 6, :row => 2, :sticky => 'w')
 # create the button with value %
-Tk::Tile::Button.new(content) {text '%'; command {manageaction('%')}}.grid( :column => 7, :row => 2, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '%'; command {manageaction('%')}}.grid( :column => 7, :row => 2, :sticky => 'w')
 
 # create the button with value 4
-Tk::Tile::Button.new(content) {text '4'; command {managedigit('+','4')}}.grid( :column => 4, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '4'; command {managedigit('+','4')}}.grid( :column => 4, :row => 3, :sticky => 'w')
 # create the button with value 5
-Tk::Tile::Button.new(content) {text '5'; command {managedigit('+','5')}}.grid( :column => 5, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '5'; command {managedigit('+','5')}}.grid( :column => 5, :row => 3, :sticky => 'w')
 # create the button with value 6
-Tk::Tile::Button.new(content) {text '6'; command {managedigit('+','6')}}.grid( :column => 6, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '6'; command {managedigit('+','6')}}.grid( :column => 6, :row => 3, :sticky => 'w')
 # create the button with value -
-Tk::Tile::Button.new(content) {text 'x'; command {manageaction('x')}}.grid( :column => 7, :row => 3, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text 'x'; command {manageaction('x')}}.grid( :column => 7, :row => 3, :sticky => 'w')
 
 # create the button with value 7
-Tk::Tile::Button.new(content) {text '7'; command {managedigit('+','7')}}.grid( :column => 4, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '7'; command {managedigit('+','7')}}.grid( :column => 4, :row => 4, :sticky => 'w')
 # create the button with value 8
-Tk::Tile::Button.new(content) {text '8'; command {managedigit('+','8')}}.grid( :column => 5, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '8'; command {managedigit('+','8')}}.grid( :column => 5, :row => 4, :sticky => 'w')
 # create the button with value 9
-Tk::Tile::Button.new(content) {text '9'; command {managedigit('+','9')}}.grid( :column => 6, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '9'; command {managedigit('+','9')}}.grid( :column => 6, :row => 4, :sticky => 'w')
 # create the button with value x
-Tk::Tile::Button.new(content) {text '-'; command {manageaction('-')}}.grid( :column => 7, :row => 4, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '-'; command {manageaction('-')}}.grid( :column => 7, :row => 4, :sticky => 'w')
 
 # create the button with value 0
-Tk::Tile::Button.new(content) {text '0'; command {managedigit('+','0')}}.grid( :column => 4, :row => 5, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '0'; command {managedigit('+','0')}}.grid( :column => 4, :row => 5, :sticky => 'w')
 # create the button with value ,
-Tk::Tile::Button.new(content) {text ','; command {add1digit('.')}}.grid( :column => 5, :row => 5, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text ','; command {add1digit('.')}}.grid( :column => 5, :row => 5, :sticky => 'w')
 # create the button with value =
-Tk::Tile::Button.new(content) {text '='; command {calcresult}}.grid( :column => 6, :row => 5, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '='; command {calcresult}}.grid( :column => 6, :row => 5, :sticky => 'w')
 # create the button with value +
-Tk::Tile::Button.new(content) {text '+'; command {manageaction('+')}}.grid( :column => 7, :row => 5, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text '+'; command {manageaction('+')}}.grid( :column => 7, :row => 5, :sticky => 'w')
 
 # create the button with value c
-Tk::Tile::Button.new(content) {text 'c'; command {resetcalc}}.grid( :column => 4, :row => 6, :sticky => 'w')
+Tk::Tile::Button.new(kassatab) {text 'c'; command {resetcalc}}.grid( :column => 4, :row => 6, :sticky => 'w')
+# ----------------------------------------------------------------------------------------
+# # end populate kassatab ----------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+# set kassatab as selected tab
+notebook.select(kassatab)
 
-TkWinfo.children(content).each {|w| TkGrid.configure w, :padx => 5, :pady => 5}
+# ----------------------------------------------------------------------------------------
+# start business logic -------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+TkWinfo.children(kassatab).each {|w| TkGrid.configure w, :padx => 5, :pady => 5}
 tl_result.focus
 enteramountscreen.bind("Return") {calcresult}
 #add overview
@@ -237,5 +257,9 @@ def updateoverview (description, action)
     $overview.value = $overview + action + description
   end
 end
+# ----------------------------------------------------------------------------------------
+# end business logic -------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
+
 
 Tk.mainloop
